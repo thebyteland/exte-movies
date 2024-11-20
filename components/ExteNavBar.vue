3e7f80be358e36b0e417efc336b501e177c95b67
Adrián Jiménez Martín <106698345+thebyteland@users.noreply.github.com>
2024-11-20 17:01:27 +0100

    [feat] Nav bar style improvement

--- components/ExteNavBar.vue
+++ components/ExteNavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="w-full">
+  <nav class="w-full rounded-sm p-4 bg-slate-800">
     <ul class="w-full flex gap-8 justify-end text-white text-lg lg:text-xl uppercase">
       <li><NuxtLink to="/" class="hover:underline">{{ $t('HOME_LINK') }}</NuxtLink></li>
       <li><NuxtLink to="my-list" class="hover:underline">{{ $t('MY_LIST_LINK') }}</NuxtLink></li>
