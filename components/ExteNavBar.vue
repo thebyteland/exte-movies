d9a718ad2b21227715fbb182c042bf96827fefea
Adrián Jiménez Martín <106698345+thebyteland@users.noreply.github.com>
2024-11-20 17:54:38 +0100

    [feat] My list section title and nav fix

--- components/ExteNavBar.vue
+++ components/ExteNavBar.vue
@@ -15,7 +15,7 @@
     <nav>
       <ul class="w-full flex gap-8 justify-end text-white text-lg lg:text-xl uppercase">
         <li><NuxtLink to="/" class="hover:underline">{{ $t('HOME_LINK') }}</NuxtLink></li>
-        <li><NuxtLink to="my-list" class="hover:underline">{{ $t('MY_LIST_LINK') }}</NuxtLink></li>
+        <li><NuxtLink to="/my-list" class="hover:underline">{{ $t('MY_LIST_LINK') }}</NuxtLink></li>
       </ul>
     </nav>
   </div>
--- pages/my-list.vue
+++ pages/my-list.vue
@@ -3,7 +3,7 @@
     <h1>{{ $t('MY_LIST_TITLE') }}</h1>
 
     <ClientOnly >
-      <ExteMoviesList v-if="moviesList && moviesList.length" :list="moviesList" title="MOVIES_LIST_SECTION_TITLE" />
+      <ExteMoviesList v-if="moviesList && moviesList.length" :list="moviesList" title="MY_MOVIES_LIST_TITLE" />
       <p v-else class="text-2xl text-center bg-white rounded-lg p-4">{{ $t('MY_MOVIES_EMPTY_LIST') }}</p>
     </ClientOnly>
   </main>
